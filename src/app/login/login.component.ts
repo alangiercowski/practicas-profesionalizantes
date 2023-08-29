45472f5d93de9abc94a532f15d410479d7fd51de
alejogonzalez2 <81708738+alejogonzalez2@users.noreply.github.com>
2023-08-29 14:32:23 -0300

    login con google
    no se como mandar algunos archivos que esten en distintas carpetas, asi que va todo :p

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,6 +1,22 @@
 import { Component } from '@angular/core';
 import { Route, Router } from '@angular/router';
 import { HttpService } from '../http-service.service';
+import {initializeApp} from 'firebase/app'
+import {getFirestore, collection, getDocs} from 'firebase/firestore/lite'
+import {getAuth, signInWithPopup, GoogleAuthProvider} from 'firebase/auth'
+
+const fbConfig = {
+  apiKey: "AIzaSyDVOMc24POg6wNOO-WU3YKixGI4VPctRc8",
+  authDomain: "pract-83206.firebaseapp.com",
+  projectId: "pract-83206",
+  storageBucket: "pract-83206.appspot.com",
+  messagingSenderId: "223218284293",
+  appId: "1:223218284293:web:10ab6e945dd7d4b12ee96c"
+};
+
+const app = initializeApp(fbConfig)
+const provider = new GoogleAuthProvider()
+const auth = getAuth()
 
 @Component({
   selector: 'app-login',
@@ -32,6 +48,19 @@ export class LoginComponent {
         this.error = error.error
       }
     });
+  }
+  public loginGoogle(){
+    signInWithPopup(auth, provider).then((r) => {
+      const credential = GoogleAuthProvider.credentialFromResult(r)
+      const token = credential?.accessToken
+      const usr = r.user
+      this.router.navigate([""])
+    }).catch((error)=>{
+      const errCode = error.errCode
+      const errMessage = error.errMessage
+      const email = error.customData.email
+      const credential = GoogleAuthProvider.credentialFromError(error)
+    })
   }
 
 }
